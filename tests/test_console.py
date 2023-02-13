022ac1d5de50168c2f7152ac4f4b9d66a3547b0d
addisu30 <105362027+addisu30@users.noreply.github.com>
2023-02-13 12:03:44 +0300

    Update test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -13,9 +13,9 @@ Unittest classes:
 import os
 import sys
 import unittest
-from ..models import storage
-from ..models.engine.file_storage import FileStorage
-from ..console import HBNBCommand
+from models import storage
+from models.engine.file_storage import FileStorage
+from console import HBNBCommand
 from io import StringIO
 from unittest.mock import patch
 
